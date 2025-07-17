a9fc995d75814b5e2fe0283c2e6ff65f97ee99e6
Francesco Scalia <fscalia93@gmail.com>
2025-07-17 14:32:24 +0200

    fix basket

--- main/config/baskets/main.py
+++ main/config/baskets/main.py
@@ -1,13 +1,13 @@
 from config.baskets.DAX40 import DAX40
-from config.baskets.EUROPE_STOXX_600 import EUROPE_STOX_600
+from config.baskets.EUROPE_STOXX_600 import EURO600
 from config.baskets.NASDAQ100 import NADAQ
-from config.baskets.nikkei import NIKKEI
+from config.baskets.nikkei import nikkei225
 from config.baskets.SP500 import SP500
 
 BASKETS = [
     {"NASDAQ": NADAQ},
     {"DAX": DAX40},
-    {"EURO_STOX": EUROPE_STOX_600},
-    {"NIKKEY": NIKKEI},
+    {"EURO_STOX": EURO600},
+    {"NIKKEY": nikkei225},
     {"SP500": SP500},
 ]
