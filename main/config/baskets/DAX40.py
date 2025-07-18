0e0de8f7f65c6275d4b7b219bcdb81354e1d6f85
Francesco Scalia <fscalia93@gmail.com>
2025-07-18 19:25:36 +0200

    fix dax40

--- main/config/baskets/DAX40.py
+++ main/config/baskets/DAX40.py
@@ -1,45 +1,43 @@
 DAX40 = [
-    "ADS.DE",   # Adidas
-    "AIR.DE",   # Airbus
-    "ALV.DE",   # Allianz
-    "BAS.DE",   # BASF
+    "ADS.DE",  # Adidas
+    "AIR.DE",  # Airbus
+    "ALV.DE",  # Allianz
+    "BAS.DE",  # BASF
     "BAYN.DE",  # Bayer
-    "BEI.DE",   # Beiersdorf
-    "BMW.DE",   # BMW
-    "BNR.DE",   # Brenntag
-    "CBK.DE",   # Commerzbank
-    "CON.DE",   # Continental
-    "DBK.DE",   # Deutsche Bank
-    "DB1.DE",   # Deutsche Börse
-    "DHL.DE",   # DHL Group (vormals Deutsche Post)
-    "DTE.DE",   # Deutsche Telekom
+    "BEI.DE",  # Beiersdorf
+    "BMW.DE",  # BMW
+    "BNR.DE",  # Brenntag
+    "CBK.DE",  # Commerzbank
+    "CON.DE",  # Continental
+    "DBK.DE",  # Deutsche Bank
+    "DB1.DE",  # Deutsche Börse
+    "DTE.DE",  # Deutsche Telekom
     "EOAN.DE",  # E.ON
-    "FRE.DE",   # Fresenius
-    "FME.DE",   # Fresenius Medical Care
+    "FRE.DE",  # Fresenius
+    "FME.DE",  # Fresenius Medical Care
     "HNR1.DE",  # Hannover Rück
-    "HEI.DE",   # Heidelberg Materials
+    "HEI.DE",  # Heidelberg Materials
     "HEN3.DE",  # Henkel VZ
-    "IFX.DE",   # Infineon
-    "MBG.DE",   # Mercedes-Benz
-    "MRK.DE",   # Merck
-    "MTX.DE",   # MTU Aero Engines
+    "IFX.DE",  # Infineon
+    "MBG.DE",  # Mercedes-Benz
+    "MRK.DE",  # Merck
+    "MTX.DE",  # MTU Aero Engines
     "MUV2.DE",  # Munich Re
     "PAH3.DE",  # Porsche AG Vz
-    "QIA.DE",   # Qiagen
-    "RHM.DE",   # Rheinmetall
-    "RWE.DE",   # RWE
-    "SAP.DE",   # SAP
+    "QIA.DE",  # Qiagen
+    "RHM.DE",  # Rheinmetall
+    "RWE.DE",  # RWE
+    "SAP.DE",  # SAP
     "SRT3.DE",  # Sartorius VZ
-    "SIE.DE",   # Siemens
-    "ENR.DE",   # Siemens Energy
-    "SHL.DE",   # Siemens Healthineers
-    "SY1.DE",   # Symrise
+    "SIE.DE",  # Siemens
+    "ENR.DE",  # Siemens Energy
+    "SHL.DE",  # Siemens Healthineers
+    "SY1.DE",  # Symrise
     "VOW3.DE",  # Volkswagen VZ
-    "VNA.DE",   # Vonovia
-    "ZAL.DE",   # Zalando
-    "DTG.DE",   # Daimler Truck
-    "HFG.DE",   # HelloFresh
-    "PUM.DE",   # Puma
+    "VNA.DE",  # Vonovia
+    "ZAL.DE",  # Zalando
+    "DTG.DE",  # Daimler Truck
+    "HFG.DE",  # HelloFresh
+    "PUM.DE",  # Puma
     "P911.DE",  # Porsche AG (operatives Unternehmen)
 ]
-
