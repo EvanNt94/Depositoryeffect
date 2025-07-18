63f54cf27c3b0b5ae8bccebe784486cb33c8ccad
Francesco Scalia <fscalia93@gmail.com>
2025-07-18 19:25:52 +0200

    add test

--- main/test/test.py
+++ main/test/test.py
@@ -3,10 +3,11 @@ from datetime import datetime, timedelta
 import pandas as pd
 import yfinance as yf
 
-tickers = "CEG"
+tickers = "DHL.DE"
 start_date = "2020-01-17"
 end_date = "2025-01-30"
 data = yf.download(tickers, start=start_date, end=end_date)
 
 
 print(data)
+print(data[data.isna().any(axis=1)])
